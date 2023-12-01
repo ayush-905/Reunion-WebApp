6fa75248c8273e4c38260da032292a1a33651dde
Ayush <triayush08@gmail.com>
2023-12-01 15:35:48 +0530

    removing unnecessary dependency

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
-const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000;
 const cors=require('cors')
 const { errorHandler } = require('./middleware/errorMiddleware');
@@ -11,7 +10,6 @@ connectDB();
 
 const app = express();
 
-app.use(bodyParser.json());
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
